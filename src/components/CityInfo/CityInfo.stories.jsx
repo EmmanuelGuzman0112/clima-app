c3e6a53fb7e7e127135fb96080040f70e148d07e
HSNBRUT11\Emmanuel Guzman <guzman-emmanuel@live.com>
2021-07-21 12:19:07 -0300

    subo storybook de componentes

---
+++ src/components/CityInfo/CityInfo.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'typeface-roboto'
+import CityInfo from './CityInfo'
+
+export default {
+    title: "CityInfo",
+    component: CityInfo
+}
+
+export const CityExample =  () => <CityInfo city={"Buenos Aires"} country={"Arg"}></CityInfo>
